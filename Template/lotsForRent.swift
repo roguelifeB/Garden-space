0a6a325ad5292b33ac05e00c73d8ed97bb31423a
StreetCode Academy <streetcodeacademy@SCA4StreetCodes-MacBook-Air.local>
2017-04-19 20:33:10 -0700

    Done for the today, added navigation and a buy button

---
+++ Template/lotsForRent.swift
@@ -0,0 +1,29 @@
+//
+//  lotsForRent.swift
+//  Template
+//
+//  Created by StreetCode Academy on 4/19/17.
+//  Copyright © 2017 StreetCode. All rights reserved.
+//
+
+import UIKit
+
+class lotsForRent: UIViewController {
+    
+
+    @IBAction func buy(_ sender: Any) {
+        print("buy tapped");
+    }
+    override func viewDidLoad() {
+        
+    }
+    
+    
+    
+    
+    func buttonTapped(sender: UIButton) {
+        print ("button tapped");
+        
+    }
+    
+}
