94d52a2cee82aeda86bc895f6f5626c883f36bfd
StreetCode Academy <streetcodeacademy@SCA4StreetCodes-MacBook-Air.local>
2017-05-10 20:22:28 -0700

    made produce profile and added images

---
+++ Template/ProduceCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProduceCollectionViewCell.swift
+//  Template
+//
+//  Created by StreetCode Academy on 5/10/17.
+//  Copyright © 2017 StreetCode. All rights reserved.
+//
+
+import UIKit
+
+class ProduceCollectionViewCell: UICollectionViewCell {
+    
+}
